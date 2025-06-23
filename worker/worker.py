2f24e1dc5e1ddd7429e58ab1f969702c191e2b12
Sam <singbar00@gmail.com>
2025-06-23 14:01:22 -0700

    fix typo

--- worker/worker.py
+++ worker/worker.py
@@ -2,19 +2,16 @@ import os
 import asyncio
 from temporalio.client import Client
 from temporalio.worker import Worker
-
 from activities.scraper_activity    import fetch_best_trade_route
 from activities.ship_loader_activity import load_ships_from_file
 from activities.db_activity         import upsert_route_to_db
-from activities.uex_activity        import fetch_and_store_uex_data
 from workflows.hauling_workflow      import HaulingWorkflow
 from workflows.daily_update_workflow import DailyUpdateWorkflow
-from workflows.uex_refresh_workflow import UexRefreshWorkflow
+
 async def main():
     address = os.getenv("TEMPORAL_ADDRESS", "us-east-1.aws.api.temporal.io:7233")
-    namespace = os.getenv("TEMPORAL_NAMESPACE", "quickstart-singbar00-e2d9a520.xfqa6")
+    namespace = os.getenv("TEMPORAL_NAMESPACE", "default")
     api_key = os.getenv("TEMPORAL_API_KEY")
-    uex_key = os.getenv("UEX_KEY")
 
     print(f"addrress={address}")
     print(f"api_key={api_key}")
@@ -29,8 +26,8 @@ async def main():
     worker = Worker(
         client,
         task_queue="hauling-task-queue",
-        workflows=[DailyUpdateWorkflow, HaulingWorkflow,UexRefreshWorkflow],
-        activities=[fetch_best_trade_route, upsert_route_to_db, load_ships_from_file,fetch_and_store_uex_data],
+        workflows=[DailyUpdateWorkflow, HaulingWorkflow],
+        activities=[fetch_best_trade_route, upsert_route_to_db, load_ships_from_file],
         max_concurrent_activities=5
     )
     await worker.run()
